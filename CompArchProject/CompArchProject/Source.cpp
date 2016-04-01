a40db1705833b6e19126d50bc748c07594c42f4e
Hegman-Lap\Hegman <arh426@msstate.edu>
2016-04-01 11:22:17 -0500

    Worked on some more stuff.

--- CompArchProject/CompArchProject/Source.cpp
+++ CompArchProject/CompArchProject/Source.cpp
@@ -51,8 +51,33 @@ struct INSTRUCTION{
 	//to do. I don't know if Dr. Sherif will be happy with the second
 	//idea thoug...
 };
+
+struct REGISTER{
+	int number;		//this is the memory position
+	int value;
+};
+
+
 void InstructionFetch(string[], int, INSTRUCTION);
-void InstructionDecode();
+void InstructionDecode(INSTRUCTION);
+void InstructionExecute(INSTRUCTION);
+void InstructionExecute_R(INSTRUCTION);
+void InstructionExecute_I(INSTRUCTION);
+void InstructionExecute_J(INSTRUCTION);
+
+void RegisterInit();
+
+REGISTER zero;
+REGISTER a0;
+REGISTER a1;
+REGISTER v0;
+REGISTER v1;
+REGISTER v2;
+REGISTER v3;
+REGISTER t0;
+REGISTER t1;
+REGISTER t2;
+REGISTER t3;
 /*
 //This is the logic I came up with, but IDK where it should go..
 switch(OpCode){
@@ -152,3 +177,110 @@ void InstructionDecode(INSTRUCTION currInstruction){
 		currInstruction.type = J;
 	}
 }
+
+void InstructionExecute(INSTRUCTION currInstruction){
+	switch(currInstruction.type){
+		case R:
+			InstructionExecute_R(currInstruction);
+			break;
+		case I:
+			InstructionExecute_I(currInstruction);
+			break;
+		case J:
+			InstructionExecute_J(currInstruction);
+			break;
+	}
+}
+
+void InstructionExecute_R(INSTRUCTION currInstruction){
+	switch(currInstruction.func){
+		case ADD:
+			currInstruction.tempMem = currInstruction.rs + currInstruction.rt;
+			break;
+		case SUB:
+			currInstruction.tempMem = currInstruction.rs - currInstruction.rt;
+			break;
+		case SLL:
+			currInstruction.tempMem = currInstruction.rs << currInstruction.rt;
+			break;
+		case SRL:
+			currInstruction.tempMem	= currInstruction.rs >> currInstruction.rt;
+			break;
+		case AND:
+			currInstruction.tempMem = currInstruction.rs & currInstruction.rt;
+			break;
+		case NOR:
+			currInstruction.tempMem = ~(currInstruction.rs | currInstruction.rt);
+			break;
+		case OR:
+			currInstruction.tempMem = currInstruction.rs | currInstruction.rt;
+			break;
+		case XOR:
+			currInstruction.tempMem = currInstruction.rs ^ currInstruction.rt;
+			break;
+	}
+}
+
+void InstructionExecute_I(INSTRUCTION currInstruction){
+	switch(currInstruction.func){
+		case ADDI:
+			currInstruction.tempMem = currInstruction.rs + currInstruction.immediate;
+			break;
+		case ORI:
+			currInstruction.tempMem = currInstruction.rs - currInstruction.immediate;
+			break;
+		case LW:
+			
+			break;
+		case SW:
+			
+			break;
+		case BEQ:
+			if(currInstruction.rs == currInstruction.rt){
+				//need to change PC here
+			}
+			break;
+		case BLTZ:
+			if(currInstruction.rs < zero.value){
+				//need to change PC here
+			}
+			break;
+		case BLT:
+			if(currInstruction.rs < currInstruction.rt){
+				//need to change PC here
+			}
+			break;
+	}
+}
+
+void InstructionExecute_J(INSTRUCTION currInstruction){
+	//This will only be a jump instruction
+	//need to change PC here
+}
+
+void RegisterInit(){
+	zero.number = 0x00;
+	zero.value = 0x00;
+
+	a0.number = 0x01;
+
+	a1.number = 0x02;
+
+	v0.number = 0x03;
+
+	v1.number = 0x04;
+
+	v2.number = 0x05;
+
+	v3.number = 0x06;
+
+	t0.number = 0x07;
+
+	t1.number = 0x08;
+
+	t2.number = 0x09;
+
+	t3.number = 0x0A;
+}
+//So Dr Sherif was saying that if you try to write to the zero register, nothing happens, it just doesn't write to the zero register. To emulate this behavior, I think we should just check
+//the destination register in the WB stage, and if the destination register is the zero register, we just ignore the instruction and carry on. Thoughts? 
